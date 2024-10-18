4443805a7ff70470cb38142e965e13675b00e503
MineKing <69981672+MineKing9534@users.noreply.github.com>
2024-10-18 14:39:13 +0200

    Add discord TypeMapper for generic snowflake (#3)

---
+++ discord/src/test/kotlin/tests/discord/Snowflake.kt
@@ -0,0 +1,59 @@
+package tests.discord
+
+import de.mineking.database.*
+import de.mineking.database.vendors.PostgresConnection
+import de.mineking.database.vendors.PostgresMappers
+import net.dv8tion.jda.api.entities.*
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
+import net.dv8tion.jda.api.entities.emoji.CustomEmoji
+import net.dv8tion.jda.api.entities.emoji.RichCustomEmoji
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import setup.ConsoleSqlLogger
+import setup.createJDA
+import setup.createSnowflake
+import setup.recreate
+
+data class SnowflakeDao(
+	@AutoGenerate @Key @Column val id: Int = 0,
+	@Column val snowflake: ISnowflake? = null,
+)
+
+class SnowflakeTest {
+	val connection = PostgresConnection("localhost:5432/test", user = "test", password = "test")
+	val table: Table<SnowflakeDao>
+
+	val guilds = listOf(
+		createSnowflake<Guild>(1),
+		createSnowflake<Guild>(2)
+	)
+	init {
+		//Use STRING mappers because they are more likely to fail
+		connection.registerDiscordStringMappers(createJDA(guilds), PostgresMappers.STRING, PostgresMappers.ENUM)
+		table = connection.getTable(name = "snowflake_test") { SnowflakeDao() }
+
+		table.recreate()
+
+		table.insert(SnowflakeDao(snowflake = guilds[0]))
+
+		connection.driver.setSqlLogger(ConsoleSqlLogger)
+	}
+
+	@Test
+	fun selectAll() {
+		val result = table.select().list()
+		assertEquals(1, result.size)
+		assertEquals(guilds[0], result[0].snowflake)
+	}
+
+	@Test
+	fun selectColumn() {
+		assertEquals(guilds[0], table.select<Guild>(property("snowflake")).first())
+	}
+
+	@Test
+	fun selectCondition() {
+		assertEquals(1, table.selectRowCount(where = property("snowflake") isEqualTo value(guilds[0])))
+		assertEquals(0, table.selectRowCount(where = property("snowflake") isEqualTo value(guilds[1])))
+	}
+}
