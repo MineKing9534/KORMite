026c9251f6b3569e463d2fbe352a4f0674b4d795
MineKing <69981672+MineKing9534@users.noreply.github.com>
2024-10-21 10:14:58 +0200

    Improve result handling and add QueryResult#stream (#6)

--- core/src/main/kotlin/de/mineking/database/Result.kt
+++ core/src/main/kotlin/de/mineking/database/Result.kt
@@ -3,15 +3,9 @@ package de.mineking.database
 import org.jdbi.v3.core.result.ResultIterable
 import java.sql.ResultSet
 import java.sql.SQLException
+import java.util.stream.Stream
 
 data class ReadContext(val instance: Any?, val table: TableStructure<*>, val set: ResultSet, val selected: List<String>?, val prefix: Array<String> = emptyArray(), val autofillPrefix: (String) -> Boolean = { true }, var shouldRead: Boolean = true) {
-    fun proceed(): Boolean {
-        if (!shouldRead) return true
-
-        shouldRead = false
-        return set.next()
-    }
-
     fun formatName(name: String) = ((prefix.takeIf { it.isNotEmpty() || !autofillPrefix(name) } ?: arrayOf(table.name)) + name).joinToString(".")
 
     fun <T> read(name: String, reader: (ResultSet, String) -> T): T = reader(set, formatName(name))
@@ -28,38 +22,21 @@ interface QueryResult<T> {
         return try { first() }
         catch (_: NoSuchElementException) { null }
     }
+
+    fun stream(): Stream<T>
+    fun <R> withStream(handler: (Stream<T>) -> R): R = stream().use(handler)
 }
 
-interface RowQueryResult<T: Any> : QueryResult<T> {
-    val instance: () -> T
-    fun <O> execute(handler: ((T) -> Boolean) -> O): O
-
-    override fun list(): List<T> = execute { read ->
-        val result = arrayListOf<T>()
-
-        while (true) {
-            val obj = instance()
-
-            if (!read(obj)) break
-            result.add(obj)
-        }
-
-        result
-    }
-
-    override fun first(): T = execute { read ->
-        val obj = instance()
-
-        if (!read(obj)) throw NoSuchElementException()
-        else obj
-    }
-}
-
-interface ValueQueryResult<T>: QueryResult<T> {
+interface SimpleQueryResult<T>: QueryResult<T> {
     fun <O> execute(handler: (ResultIterable<T>) -> O): O
 
     override fun list(): List<T> = execute { it.list() }
     override fun first(): T = execute { it.first() }
+
+    /**
+     * Note: The returned stream has to be closed by the user!
+     */
+    override fun stream(): Stream<T> = execute { it.stream() }
 }
 
 data class UpdateResult<T>(
