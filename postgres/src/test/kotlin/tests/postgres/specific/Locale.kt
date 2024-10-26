d38b2a50ecf7b81eea31c2cbd9e46829621628b2
MineKing <mineking9534@gmail.com>
2024-10-26 22:20:07 +0200

    Add missing tests

---
+++ postgres/src/test/kotlin/tests/postgres/specific/Locale.kt
@@ -0,0 +1,36 @@
+package tests.postgres.specific
+
+import de.mineking.database.*
+import de.mineking.database.vendors.postgres.PostgresConnection
+import org.junit.jupiter.api.Test
+import setup.ConsoleSqlLogger
+import setup.recreate
+import java.awt.Color
+import java.time.Instant
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
+import java.util.*
+import kotlin.test.assertEquals
+
+data class LocaleDao(
+	@AutoIncrement @Key @Column val id: Int = 0,
+	@Column val locale: Locale = Locale.ENGLISH,
+)
+
+class LocaleTest {
+	val connection = PostgresConnection("localhost:5432/test", user = "test", password = "test")
+	val table = connection.getTable(name = "locale_test") { LocaleDao() }
+
+	init {
+		table.recreate()
+
+		table.insert(LocaleDao(locale = Locale.GERMAN))
+
+		connection.driver.setSqlLogger(ConsoleSqlLogger)
+	}
+
+	@Test
+	fun selectAll() {
+		assertEquals(Locale.GERMAN, table.select<Locale>(property("locale")).first())
+	}
+}
